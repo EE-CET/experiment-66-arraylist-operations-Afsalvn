82c97bc3ac57cb04d14f44d771fc242303a0fe42
Afsalvn <147943587+Afsalvn@users.noreply.github.com>
2026-04-10 15:42:18 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine().trim());
+        String[] items = sc.nextLine().trim().split(" ");
+        String search = sc.nextLine().trim();
+
+        ArrayList<String> cart = new ArrayList<>(Arrays.asList(items));
+        Collections.sort(cart);
+
+        System.out.println("Sorted Items: " + cart);
+        System.out.println(cart.contains(search) ? "Found" : "Not Found");
+    }
+}
